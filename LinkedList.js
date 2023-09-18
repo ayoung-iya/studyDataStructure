b99c83685c375b1ef1bb25d09b037638720c3c5e
ayoung-iya <rhaehfdl1879@naver.com>
2023-09-19 00:14:40 +0900

    LinkedList를 구현하기 위해 노드를 구현하기 위한 함수와 LinkedList를 구현하기 위한 함수를 작성

---
+++ LinkedList.js
@@ -0,0 +1,12 @@
+// Node() : data와 point를 가지고 있는 객체
+function Node(data) {
+  this.data = data;
+  this.next = null;
+}
+
+// LinkedList() : head와 length를 가지고 있는 객체
+function LinkedList() {
+  this.head = null;
+  this.length = 0;
+}
+
