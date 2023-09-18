0c86db2fb5d883643000adc90dfacc53c6da5952
ayoung-iya <rhaehfdl1879@naver.com>
2023-09-19 01:17:27 +0900

    강의를 따라하며 append 메서드 수정 ?? 36줄 this.head 대신 current를 넣으면 왜 안될까? ? 테스트 코드로 확인을 했는데 size 메서드를 했을 때 올바른 반환값이 나오지 않는 것을 확인 -> 원인은 size 반환값이 LinkedList.length였기 때문 -> this.length로 고쳤다.

--- LinkedList.js
+++ LinkedList.js
@@ -12,46 +12,45 @@ function LinkedList() {
 
 // size() : 연결 리스트 내 노드 개수 확인
 LinkedList.prototype.size = function () {
-  return LinkedList.length;
-}
+  return this.length;
+};
 
 // isEmpty() : 객체 내 노드 존재 여부 파악
 LinkedList.prototype.isEmpty = function () {
-  return LinkedList.length === 0;
-}
+  return this.length === 0;
+};
 
 // printNode() : 노드 출력
 LinkedList.prototype.printNode = function () {
   for (let node = this.head; node !== null; node = node.next) {
-    process.stdout.write(`${node.data} -> `)
+    process.stdout.write(`${node.data} -> `);
   }
-  console.log('null');
-}
+  console.log("null");
+};
 
 // append() : 연결 리스트 가장 끝에 노드 추가
 LinkedList.prototype.append = function (newData) {
-  const newNode = new Node(newData)
-  for (let node = this.head; ; node = node.next) {
-    if(node.next === null) {
-      node.next = newNode;
-      break;
+  const newNode = new Node(newData);
+  let current = this.head;
+
+  if (this.head === null) {
+    this.head = newNode;
+  } else {
+    while (current.next !== null) {
+      current = current.next;
     }
+    current.next = newNode;
   }
-}
+
+  this.length++;
+};
 
 // Test code
-const ll = new LinkedList();
-console.log(ll);
+let ll = new LinkedList();
 
-ll.head = new Node(123);
-ll.length++;
-console.log(ll);
-
-ll.head.next = new Node(456);
-ll.length++;
-console.log(ll);
+ll.append(1);
+ll.append(10);
+ll.append(100);
 
 ll.printNode();
-
-ll.append(789);
-ll.printNode();
+console.log(ll.size());
