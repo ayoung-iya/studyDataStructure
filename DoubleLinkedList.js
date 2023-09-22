59dfc80b70efd917c715f54c9fb54fd34fae21f3
ayoung-iya <rhaehfdl1879@naver.com>
2023-09-23 00:44:56 +0900

    size(), isEmpty() 메서드 구현

--- DoubleLinkedList.js
+++ DoubleLinkedList.js
@@ -11,3 +11,30 @@ function DoubleLinkedList() {
   this.tail = null;
   this.length = 0;
 }
+
+// size(): 연결 리스트 내 노드 개수 확인
+DoubleLinkedList.prototype.size = function () {
+  return this.length;
+};
+
+// isEmpty(): 객체 내 노드 존재 여부 확인
+DoubleLinkedList.prototype.isEmpty = function () {
+  return this.length === 0;
+};
+
+let dll = new DoubleLinkedList();
+let node;
+console.log(dll);
+
+node = new Node(123);
+dll.head = node;
+dll.tail = node;
+dll.length++;
+console.log(dll);
+
+node = new Node(456);
+dll.tail.next = node;
+node.prev = dll.tail;
+dll.tail = node;
+dll.length++;
+console.log(dll);
