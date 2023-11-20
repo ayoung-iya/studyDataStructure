78bdcdb35d7ff1f7bc3bddf230c14c72c9614bbf
ayoung-iya <rhaehfdl1879@naver.com>
2023-11-20 15:12:11 +0900

    FEAT: push(): 데이터 추가 FEAT: pop(): 데이터 삭제 FEAT: peek(): 가장 끝 데이터 반환 FEAT: size(): 스택 내 데이터 개수 확인 TEST: test  코드 수정

--- Stack.js
+++ Stack.js
@@ -3,24 +3,44 @@ function Stack(array) {
   this.array = array ? array : [];
 }
 
-// getBuffer() : 객체 내 데이터 셋 변환
+// getBuffer(): 객체 내 데이터 셋 변환
 Stack.prototype.getBuffer = function () {
   return this.array.slice();
 };
 
-// isEmpty() : 객체 내 데이터 o/x
+// isEmpty(): 객체 내 데이터 o/x
 Stack.prototype.isEmpty = function () {
   return !this.array.length;
 };
 
-//testCase
+// push(): 데이터 추가
+Stack.prototype.push = function (element) {
+  return this.array.push(element);
+};
+
+// pop(): 데이터 삭제
+Stack.prototype.pop = function () {
+  return this.array.pop();
+};
+
+// peek(): 가장 끝 데이터 반환
+Stack.prototype.peek = function () {
+  return this.array[this.array.length - 1];
+};
+
+// size(): 스택 내 데이터 개수 확인
+Stack.prototype.size = function () {
+  return this.array.length;
+};
+
+// testCase
 let stack = new Stack([1, 2, 3]);
 
 console.log(stack);
+stack.push(4);
+console.log(stack);
 
-let data = stack.getBuffer();
-console.log(data);
-console.log(data === stack.array); // output: false -> 데이터가 복사되었다
-
-console.log(stack.isEmpty());
-console.log(Object.getOwnPropertyDescriptors(Stack.prototype));
+console.log(stack.pop());
+console.log(stack.pop());
+console.log(stack.peek());
+console.log(stack.size());
