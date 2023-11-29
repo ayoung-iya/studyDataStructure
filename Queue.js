8fd60d9f0fb1b305ffa0d2efbaf2b4c92225bcdb
ayoung-iya <rhaehfdl1879@naver.com>
2023-11-29 18:20:35 +0900

    FEAT: Queue.prototype.front - 가장 첫 데이터 반환 FEAT: Queue.prototype.size - 큐 내 데이터 갯수 확인 FEAT: Queue.prototype.clear - 큐 초기화 TEST: test case 수정

--- Queue.js
+++ Queue.js
@@ -23,14 +23,29 @@ Queue.prototype.dequeue = function () {
   return this.array.shift();
 };
 
+// front(): 가장 첫 데이터 반환
+Queue.prototype.front = function () {
+  return this.array[0];
+};
+
+// size(): 큐 내 데이터 갯수 확인
+Queue.prototype.size = function () {
+  return this.array.length;
+};
+
+// clear(): 큐 초기화
+Queue.prototype.clear = function () {
+  this.array = [];
+};
+
 //test case
-let queue = new Queue([1, 2]);
+let queue = new Queue([1, 2, 3, 4]);
+
+queue.dequeue();
+console.log(queue.front());
 console.log(queue);
 
-queue.enqueue(3);
-queue.enqueue(4);
+console.log(queue.size());
+queue.clear();
 console.log(queue);
-
-console.log(queue.dequeue());
-console.log(queue.dequeue());
-console.log(queue);
+console.log(queue.size());
